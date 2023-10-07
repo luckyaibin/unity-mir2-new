db7f43baf20161e9d6b45a4b869b96db16820a93
none <none@none.com>
2023-10-07 13:53:55 +0800

    * 增加限时测试人物8方向动画

--- Assets/Script/Map/MapLoad.cs
+++ Assets/Script/Map/MapLoad.cs
@@ -8,9 +8,10 @@ using UnityEditor;
 using System.Linq;
 
 
-
+[Serializable]
 public class MapLoad : MonoBehaviour
 {
+    [SerializeField]
     Tilemap ground, grass;
 
     [SerializeField]
@@ -22,68 +23,69 @@ public class MapLoad : MonoBehaviour
     {
         if (ground == null)
         {
-            ground = GameObject.Find("Grid/Ground").GetComponent<Tilemap>();
+            ground = GameObject.Find("Map/Ground").GetComponent<Tilemap>();
         }
-        if (grass == null)
-        {
-            grass = GameObject.Find("Grid/Grass").GetComponent<Tilemap>();
-        }
+        // if (grass == null)
+        // {
+        //     grass = GameObject.Find("Grid/Grass").GetComponent<Tilemap>();
+        // }
     }
     [ContextMenu("SaveMap")]
-    private void SaveMap()
+    public void SaveMap()
     {
         Multiple multiple = new Multiple();
 
-        SingleTilemap groud = new SingleTilemap();
-        groud.tilemapName = "Ground";
-        var v3size = ground.size;
-        for (int i = 0; i < v3size.x; i++)
+        SingleTilemap singleGround = new SingleTilemap();
+        singleGround.tilemapName = "Ground";
+        var v3size = this.ground.size;
+
+        for (int i = this.ground.cellBounds.min.x; i < this.ground.cellBounds.max.x; i++)
         {
-            for (int j = 0; j < v3size.y; j++)
+            for (int j = this.ground.cellBounds.min.y; j < this.ground.cellBounds.max.y; j++)
             {
                 var pos = new Vector3Int(i, j, 0);
                 var tileBase = ground.GetTile(pos);
                 if (tileBase != null)
                 {
                     var t = new SingleTile();
-                    t.tileName = tileBase.name;
+                   // t.n = tileBase.name;
                     t.x = i;
                     t.y = j;
-                    groud.singleTilemaps.Add(t);
+                    singleGround.singleTilemaps.Add(t);
                 }
             }
         }
 
 
-        SingleTilemap grass = new SingleTilemap();
-        grass.tilemapName = "Grass";
-        var grassV3size = ground.size;
-        for (int i = 0; i < grassV3size.x; i++)
-        {
-            for (int j = 0; j < grassV3size.y; j++)
-            {
-                var pos = new Vector3Int(i, j, 0);
-                var tileBase = ground.GetTile(pos);
-                if (tileBase != null)
-                {
-                    var t = new SingleTile();
-                    t.tileName = tileBase.name;
-                    t.x = i;
-                    t.y = j;
-                    grass.singleTilemaps.Add(t);
-                }
-            }
-        }
+        // SingleTilemap singleGrass = new SingleTilemap();
+        // singleGrass.tilemapName = "Grass";
+        // var grassV3size = this.grass.size;
+        // for (int i = 0; i < grassV3size.x; i++)
+        // {
+        //     for (int j = 0; j < grassV3size.y; j++)
+        //     {
+        //         var pos = new Vector3Int(i, j, 0);
+        //         var tileBase = ground.GetTile(pos);
+        //         if (tileBase != null)
+        //         {
+        //             var t = new SingleTile();
+        //             t.tileName = tileBase.name;
+        //             t.x = i;
+        //             t.y = j;
+        //             singleGrass.singleTilemaps.Add(t);
+        //         }
+        //     }
+        // }
 
-        multiple.Add(groud);
-        multiple.Add(grass);
+        multiple.Add(singleGround);
+        // multiple.Add(singleGrass);
 
         // json
         var jsonData = JsonUtility.ToJson(multiple);
         var bytes = System.Text.Encoding.ASCII.GetBytes(jsonData);
         FileStream file = File.Create(Application.streamingAssetsPath + "/" + MapName + ".json");
-        file.Dispose();
         file.Write(bytes, 0, bytes.Length);
+        file.Dispose();
     }
 
     [ContextMenu("LoadMap")]
@@ -94,29 +96,47 @@ public class MapLoad : MonoBehaviour
         for (int m = 0; m < multiple.Count; m++)
         {
             var singleTilemap = multiple.Get(m);
-            var tmc = GameObject.Find("Grid/" + singleTilemap.tilemapName).GetComponent<Tilemap>();
-
+            var tmc = GameObject.Find("Map/" + singleTilemap.tilemapName+"Loaded").GetComponent<Tilemap>();
+            tmc.ClearAllTiles();
             for (int i = 0; i < singleTilemap.singleTilemaps.Count; i++)
             {
                 var tileInfo = singleTilemap.singleTilemaps[i];
-                YourTileClass tileInstance = ScriptableObject.CreateInstance<YourTileClass>();
-                Texture2D tex = Resources.Load<Texture2D>("tileTexture") as Texture2D;
-                Sprite sprite = Sprite.Create(tex, new Rect(0, 0, 400, 400), new Vector2(0.5f, 0.5f));
+                MyTileClass tileInstance = ScriptableObject.CreateInstance<MyTileClass>();
+                //Texture2D tex = Resources.Load<Texture2D>("tileTexture") as Texture2D;
+                // Texture2D tex = Resources.Load("mir/Data/Map/WemadeMir2/Tiles/0") as Texture2D;
+                // Sprite sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.0f, 1.0f));
+                // var ppu = sprite.pixelsPerUnit;
+                // tileInstance.sprite = sprite;
+                Sprite sprite = Resources.Load<Sprite>("mir/Data/Map/WemadeMir2/Tiles/0");
                 tileInstance.sprite = sprite;
-                tmc.SetTile(new Vector3Int(tileInfo.x,tileInfo.y, 0), tileInstance);
+
+                tmc.SetTile(new Vector3Int(tileInfo.x, tileInfo.y, 0), tileInstance);
             }
         }
     }
+
+
+    public T GetOrAddComponent<T>(GameObject go) where T : UnityEngine.Component
+    {
+        var c = go.GetComponent<T>();
+        if (c == null)
+        {
+            c = go.AddComponent<T>();
+        }
+        return c;
+    }
 }
+
 
 
 
 /// <summary>
 /// 多个瓦片地图的布局
 /// </summary>
+[Serializable]
 public class Multiple
 {
-    private List<SingleTilemap> singleTilemaps = new List<SingleTilemap>();
+    public List<SingleTilemap> singleTilemaps = new List<SingleTilemap>();
     public void Add(SingleTilemap singlemap)
     {
         this.singleTilemaps.Add(singlemap);
@@ -130,15 +150,17 @@ public class Multiple
         return this.singleTilemaps[i];
     }
 }
-
+[Serializable]
 public class SingleTilemap
 {
     public string tilemapName;
     public List<SingleTile> singleTilemaps = new List<SingleTile>();
 }
-
+[Serializable]
 public class SingleTile
 {
-    public string tileName;
+    public int l;// tile所在库目录对应起来
+    public int n;// tile索引(数字形式的名字)
     public int x, y;
+    public int w;// walkable 1可行走
 }
--- Assets/Script/Map/MyTiles.cs
+++ Assets/Script/Map/MyTiles.cs
@@ -7,6 +7,20 @@ using System.IO;
 using UnityEditor;
 using System.Linq;
 
-public class YourTileClass : Tile{
+public class MyTileClass : Tile{
+    public int l;// tile 所在库目录对应起来
+    public int n;// tile 索引(数字形式的名字)
+    public int x, y;
+    public int w;// walkable 1可行走
 
+    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+    {
+        base.RefreshTile(position, tilemap);
+    }
+
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+    {
+        return base.StartUp(position, tilemap, go);
+
+    }
 }
